6a9d3a7881764e1b578874fce11efe535f91a454
afrahriyaz <afrahriyaz@gmail.com>
2020-12-17 20:18:49 +0530

    different fnctns

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,15 +18,21 @@ class AlienInvasion:
     def run_game(self):
         """start the main loop for the game"""
         while True:
-            #Watch for keyboards and mouse events
-            for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    sys.exit()
-            #redraw the screen during each pass through loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #make the most recently drawn screen visible
-            pygame.display.flip()           
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        #Watch for keyboards and mouse events
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        #redraw the screen during each pass through loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+         #make the most recently drawn screen visible
+        pygame.display.flip()           
 
 if __name__ == '__main__':
     #make a game instance and run the game.
