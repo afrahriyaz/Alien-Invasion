3ca610f9088de724bfaaf8e8373582f37fc263b6
afrahriyaz <afrahriyaz@gmail.com>
2021-01-14 16:42:00 +0530

    bullet firing added

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """class to manage game assets and behaviour."""
@@ -14,6 +15,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         #set bg color
         self.bg_color=(56,4,73)
     
@@ -22,6 +24,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -43,6 +46,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     
     def _check_keyup_events(self,event):
@@ -52,10 +57,17 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         #redraw the screen during each pass through loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
          #make the most recently drawn screen visible
         pygame.display.flip()           
 
--- settings.py
+++ settings.py
@@ -6,3 +6,12 @@ class Settings:
         self.screen_width=1200
         self.screen_height=800
         self.bg_color=(56,4,73)
+
+        #Ship settings
+        self.ship_speed = 2.5
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255,255,0)
