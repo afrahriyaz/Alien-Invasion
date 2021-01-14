151fbc6cc3a1c296b9fbfea32be9ff8cbcbbc4a7
afrahriyaz <afrahriyaz@gmail.com>
2021-01-14 16:59:52 +0530

    ship and bullets done

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,8 +24,21 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+
+
+    def _update_bullets(self):
+        """update position of bullet and get rid of old bullets"""
+
+        #update bullet position
+        self.bullets.update()
+
+        #get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
     def _check_events(self):
         #Watch for keyboards and mouse events
@@ -59,8 +72,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         #redraw the screen during each pass through loop
